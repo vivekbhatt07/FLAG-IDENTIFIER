834c49778b029320eba4ea6f9b293c912420d8c6
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-08-08 15:38:35 +0530

    styled: app

--- flag/src/App.js
+++ flag/src/App.js
@@ -1,9 +1,45 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./App.css";
 const App = () => {
+  const flagDictionary = {
+    "🏁": "Chequered Flag",
+    "🚩": "Triangular Flag",
+    "🎌": "Crossed Flags",
+    "🏴": "Black Flag",
+    "🏳️": "White Flag",
+    "🏳️‍🌈": "Rainbow Flag",
+    "🏳️‍⚧️": "Transgender Flag",
+    "🏴‍☠️": "Pirate Flag",
+  };
+  const flagDictionaryArr = Object.keys(flagDictionary);
+
+  const [meaning, setMeaning] = useState("");
+  function onChangeHandler(event) {
+    let userInput = event.target.value;
+    if (flagDictionary[userInput] === undefined) {
+      flagDictionary[userInput] = "We don't have this in our database";
+    }
+    setMeaning(flagDictionary[userInput]);
+  }
+
+  function clickHandler(flag) {
+    setMeaning(flagDictionary[flag]);
+  }
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <h1 className="container_head">Flag Identifier:</h1>
+      <input className="container_input" onChange={onChangeHandler}></input>
+      <div className="container_meaning">{meaning}</div>
+      <div className="container_common_head">Flags we generally know :</div>
+      <div className="container_common_flags">
+        {flagDictionaryArr.map((item, index) => {
+          return (
+            <span key={index} onClick={() => clickHandler(item)}>
+              {item}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 };
